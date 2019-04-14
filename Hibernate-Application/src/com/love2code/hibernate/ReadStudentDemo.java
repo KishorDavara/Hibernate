2613780eb8d8d17a4c4503d7679a1aa8cdae4e26
Kishor Davara <kishordavara.patel49@gmail.com>
2019-04-14 20:36:39 +0530

    chore:reading and retrieving the object from the database

---
+++ Hibernate-Application/src/com/love2code/hibernate/ReadStudentDemo.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.love2code.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.love2code.hibernate.entity.Student;
+
+/**
+ * @author KDavara
+ *
+ */
+public class ReadStudentDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create a session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// create student object
+			Student student = new Student("Daffy", "Duck", "daffy@love2code.com");
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the student object into the database");
+			System.out.println(student);
+			session.save(student);
+
+			// commit transaction	
+			session.getTransaction().commit();
+
+			//read the student object from the database using primary key
+			System.out.println("Student saved. Generated Key "+ student.getId());
+			
+			//now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			//Retrieve student based on id: primary key
+			System.out.println("Getting Student with Id "+ student.getId());
+			
+			Student myStudent = session.get(Student.class, student.getId());
+			
+			System.out.println("Student data retrieved successfully "+myStudent);
+
+			//commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done.!");
+		} finally {
+			factory.close();
+		}
+		}
+	}
